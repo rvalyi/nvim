0bb446e66f088597ec690ecec5cee43b415d170c
Raphaël Valyi <rvalyi@akretion.com>
2023-07-15 00:50:07 -0300

    added python provider for UltiSnips, but Ultisnips still not working

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -3,4 +3,18 @@
  M.ui = {theme = 'nightowl'}
  M.plugins = "custom.plugins"
  M.mappings = require "custom.mappings"
+-- vim.g.python3_host_prog = '/usr/bin/python'
+-- vim.g.loaded_python3_provider = 1
+
+    local enable_providers = {
+      "python3_provider",
+      "node_provider",
+      -- and so on
+    }
+    
+    for _, plugin in pairs(enable_providers) do
+      vim.g["loaded_" .. plugin] = nil
+      vim.cmd("runtime " .. plugin)
+    end
+
  return M
