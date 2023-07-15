9f795fdb1da367613023a8347837e16e48360a17
Raphaël Valyi <rvalyi@akretion.com>
2023-07-15 00:49:20 -0300

    some more plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -25,7 +25,7 @@ local plugins = {
     end,
   },
   {
-    "tpope/vim-fugitive",
+    "tpope/vim-fugitive", lazy = false,
   },
   {
     "ThePrimeagen/harpoon",
@@ -54,8 +54,55 @@ local plugins = {
   {
     "kdheepak/lazygit.nvim", lazy = false,
   },
+  -- {
+  --   "SmiteshP/nvim-navbuddy", lazy = false,
+  -- },
   {
-    "SmiteshP/nvim-navbuddy",
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        {
+            "SmiteshP/nvim-navbuddy",
+            dependencies = {
+                "SmiteshP/nvim-navic",
+                "MunifTanjim/nui.nvim"
+            },
+            opts = { lsp = { auto_attach = true } }
+        }
+    },
   },
+  {
+    "phaazon/hop.nvim", lazy = false,
+    opts = function()
+      require'hop'.setup {}
+    end
+  },
+  --{
+   -- "simrat39/symbols-outline.nvim",
+   -- opts = function ()
+   --   require("symbols-outline").setup()
+   -- end
+  --},
+-- disabled because strange Python3 errors while the Python provider is OK
+-- see https://github.com/NvChad/NvChad/issues/1417#issuecomment-1203490842
+-- and https://github.com/SirVer/ultisnips/issues/1290
+-- {
+--   "SirVer/ultisnips", lazy = false
+-- },
+-- {
+--    "akretion/vim-odoo-snippets", lazy = false,
+--  },
+  --
+  --  I uninstalled Octo as I'm really not sure it will make me win any time
+  -- {
+  --   'pwntester/octo.nvim', lazy = false,
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-telescope/telescope.nvim',
+  --     'nvim-tree/nvim-web-devicons',
+  --   },
+  -- opts = function ()
+  --   require"octo".setup()
+  -- end
+  -- }
 }
 return plugins
